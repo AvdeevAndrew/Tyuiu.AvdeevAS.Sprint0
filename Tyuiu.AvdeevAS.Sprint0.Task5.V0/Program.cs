79677cfc9b29a374b38c0d7a5359a3d9d09d8053
Tyuiu.AvdeevAS <Tyuiu.AvdeevAS@outlook.com>
2024-09-19 17:50:54 +0500

    Task5

---
+++ Tyuiu.AvdeevAS.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.AvdeevAS.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.AvdeevAS.Sprint0.Task5.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = " + DataService.Addition(1,5));
+            Console.WriteLine("A - B = " + DataService.Subtraction(15, 5));
+            Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
+            Console.WriteLine("A / B = " + DataService.Divisoin(5, 0));
+            Console.ReadKey();
+        }
+    }
+}
